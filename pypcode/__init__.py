20d352d058c2a436645698e8d5a403c0f20ef4de
Matt Borgerson <contact@mborgerson.com>
2021-03-19 16:50:07 -0700

    Move Arch,ArchLanguage into pypcode proper

--- pypcode/__init__.py
+++ pypcode/__init__.py
@@ -3,6 +3,8 @@ import cppyy
 import sys
 import logging
 import os.path
+import xml.etree.ElementTree as ET
+from typing import Generator, Sequence
 
 log = logging.getLogger(__name__)
 
@@ -16,11 +18,88 @@ cppyy.include(os.path.join(SLEIGH_SRC_PATH, 'translate.hh'))
 cppyy.include(os.path.join(SLEIGH_SRC_PATH, 'error.hh'))
 cppyy.load_library(os.path.join(PYPCODENATIVE_PATH, 'pypcode-native.so'))
 
+
 class AssemblyEmitCacher(cppyy.gbl.AssemblyEmit):
   def dump(self, addr, mnem, body):
     self.addr = addr
     self.mnem = mnem
     self.body = body
+
+
+class ArchLanguage:
+  """
+  Encapsulates a particular language for an architecture as defined in a *.ldefs
+  file for an arch.
+  """
+
+  __slots__ = (
+    "archdir",
+    "ldef"
+    )
+
+  archdir: str
+  ldef: ET.Element
+
+  def __init__(self, archdir: str, ldef: ET.Element):
+    self.archdir = archdir
+    self.ldef = ldef
+
+  @property
+  def pspec_path(self) -> str:
+    return os.path.join(self.archdir, self.processorspec)
+
+  @property
+  def slafile_path(self) -> str:
+    return os.path.join(self.archdir, self.slafile)
+
+  @property
+  def description(self) -> str:
+    return self.ldef.find('description').text
+
+  def __getattr__(self, key):
+    if key in self.ldef.attrib:
+      return self.ldef.attrib[key]
+    super().__attr__(key)
+
+
+class Arch:
+  """
+  Encapsulates information about a particular architecture. An architecture
+  can have many 'languages'. Query the `languages` property for a list of all
+  supported languages.
+  """
+
+  __slots__ = (
+    "archpath",
+    "archname",
+    "ldefpath",
+    "ldef",
+    "languages",
+    )
+
+  archpath: str
+  archname: str
+  ldefpath: str
+  ldef: ET.ElementTree
+  languages: Sequence[ArchLanguage]
+
+  def __init__(self, archname: str, ldefpath: str):
+    self.archpath = os.path.dirname(ldefpath)
+    self.archname = archname
+    self.ldefpath = ldefpath
+    self.ldef = ET.parse(ldefpath)
+    self.languages = [ArchLanguage(self.archpath, e) for e in self.ldef.getroot()]
+
+  @classmethod
+  def enumerate(cls) -> Generator['Arch', None, None]:
+    # processors/<architecture>/data/languages/<variant>.ldefs
+    for archname in os.listdir(SLEIGH_SPECFILES_PATH):
+      langdir = os.path.join(SLEIGH_SPECFILES_PATH, archname, 'data', 'languages')
+      if not (os.path.exists(langdir) and os.path.isdir(langdir)): continue
+      for langname in os.listdir(langdir):
+        if not langname.endswith('.ldefs'): continue
+        ldefpath = os.path.join(langdir, langname)
+        yield Arch(archname, ldefpath)
 
 # Import names into this namespace
 from cppyy.gbl import Address
