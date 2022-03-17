5fb02caca32ae7059be5f4d9c039fe2e82c89f79
Matt Borgerson <contact@mborgerson.com>
2022-03-16 19:43:26 -0700

    v1.0.4

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open('README.md') as f:
 	long_description = f.read()
 
 setup(name='pypcode',
-	version='1.0.3',
+	version='1.0.4',
 	description='Python bindings to Ghidra\'s SLEIGH library',
 	long_description=long_description,
 	long_description_content_type='text/markdown',
