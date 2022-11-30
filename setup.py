002f8e75a7041a0c9dad97d6656db3fc939e12dc
Matt Borgerson <contact@mborgerson.com>
2022-11-29 20:42:23 -0700

    v1.1.2.dev0

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ except ImportError:
 
 
 setup(name='pypcode',
-      version='1.1.1',
+      version='1.1.2.dev0',
       description="Python bindings to Ghidra's SLEIGH library",
       long_description=long_description,
       long_description_content_type='text/markdown',
