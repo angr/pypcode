975d27b775431070414c1088feea93521d0dd963
Matt Borgerson <contact@mborgerson.com>
2022-11-29 17:08:25 -0700

    Make wheel optional

--- setup.py
+++ setup.py
@@ -1,33 +1,42 @@
 #!/usr/bin/env python3
 import os
-from setuptools import setup, find_packages
-from wheel.bdist_wheel import bdist_wheel
 import sys
 
-sys.path.append(os.getcwd())
+ROOT_DIR = os.path.abspath(os.path.dirname(__file__))
+sys.path.append(ROOT_DIR)
+
+from setuptools import setup
 import build_cffi
 
+
+with open(os.path.join(ROOT_DIR, 'README.md')) as f:
+    long_description = f.read()
+
+
 def add_pkg_data_dirs(pkg, dirs):
-	pkg_data = []
-	for d in dirs:
-		for root, _, files in os.walk(os.path.join(pkg, d)):
-			r = os.path.relpath(root, pkg)
-			pkg_data.extend([os.path.join(r, f) for f in files])
-	return pkg_data
-
-with open('README.md') as f:
-	long_description = f.read()
+       pkg_data = []
+       for d in dirs:
+               for root, _, files in os.walk(os.path.join(pkg, d)):
+                       r = os.path.relpath(root, pkg)
+                       pkg_data.extend([os.path.join(r, f) for f in files])
+       return pkg_data
 
 
-class bdist_wheel_abi3(bdist_wheel):
-    def get_tag(self):
-        python, abi, plat = super().get_tag()
+cmdclass = {
+    'build_ext': build_cffi.FfiPreBuildExtension
+}
 
-        if python.startswith("cp"):
-            # on CPython, our wheels are abi3 and compatible back to 3.8
-            return "cp38", "abi3", plat
-
-        return python, abi, plat
+try:
+    from wheel.bdist_wheel import bdist_wheel
+    class bdist_wheel_abi3(bdist_wheel):
+        def get_tag(self):
+            python, abi, plat = super().get_tag()
+            if python.startswith("cp"):
+                return "cp38", "abi3", plat
+            return python, abi, plat
+    cmdclass['bdist_wheel'] = bdist_wheel_abi3
+except ImportError:
+    pass
 
 
 setup(name='pypcode',
@@ -43,7 +52,6 @@ setup(name='pypcode',
       setup_requires=['cffi'],
       install_requires=['cffi'],
       cffi_modules=['build_cffi.py:ffibuilder'],
-      cmdclass=dict(build_ext=build_cffi.FfiPreBuildExtension,
-                    bdist_wheel=bdist_wheel_abi3),
-      python_requires='>=3.8'
+      cmdclass=cmdclass,
+      python_requires='>=3.8',
       )
