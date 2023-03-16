d8d2732382cb872b4bcdbdb3aa4888fa8977add4
Matt Borgerson <contact@mborgerson.com>
2023-03-16 00:23:52 -0700

    Install sleigh

--- build_cffi.py
+++ build_cffi.py
@@ -9,7 +9,7 @@ from setuptools.command.build_ext import build_ext
 
 ROOT_DIR = os.path.abspath(os.path.dirname(__file__))
 LIB_SRC_DIR = os.path.join(ROOT_DIR, "pypcode", "native")
-SLEIGH_BUILD_DIR = os.path.join(LIB_SRC_DIR, "build")
+CMAKE_BUILD_DIR = os.path.join(LIB_SRC_DIR, "build")
 
 
 class FfiPreBuildExtension(build_ext):
@@ -23,9 +23,12 @@ class FfiPreBuildExtension(build_ext):
         except OSError as exc:
             raise RuntimeError("Please install CMake to build") from exc
 
+        install_pkg_root = os.path.abspath(os.path.join(ROOT_DIR if self.inplace else self.build_lib, "pypcode"))
+        cmake_install_prefix = install_pkg_root
         cmake_config_args = [
             "-DCMAKE_VERBOSE_MAKEFILE:BOOL=ON",
             "-DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=TRUE",
+            f"-DCMAKE_INSTALL_PREFIX={cmake_install_prefix}",
         ]
         cmake_build_args = []
         if platform.system() == "Windows":
@@ -39,14 +42,15 @@ class FfiPreBuildExtension(build_ext):
             ["cmake", "--build", "build", "--parallel", "--verbose"] + cmake_build_args,
             cwd=LIB_SRC_DIR,
         )
+        subprocess.check_call(["cmake", "--install", "build"], cwd=LIB_SRC_DIR)
 
         # Build sla files
         bin_ext = ".exe" if platform.system() == "Windows" else ""
-        sleigh_bin = os.path.join(SLEIGH_BUILD_DIR, "bin", "sleigh" + bin_ext)
-        specfiles_dir = os.path.join(ROOT_DIR if self.inplace else self.build_lib, "pypcode", "processors")
+        sleigh_bin = os.path.join(install_pkg_root, "bin", "sleigh" + bin_ext)
+        specfiles_dir = os.path.join(install_pkg_root, "processors")
         subprocess.check_call([sleigh_bin, "-a", specfiles_dir])
 
-        with open(os.path.join(SLEIGH_BUILD_DIR, "csleigh.i"), encoding="utf-8") as f:
+        with open(os.path.join(CMAKE_BUILD_DIR, "csleigh.i"), encoding="utf-8") as f:
             ffi.cdef(f.read())
 
 
@@ -62,7 +66,7 @@ def ffibuilder():
         """,
         libraries=["csleigh"] + LIBS,
         include_dirs=[LIB_SRC_DIR],
-        library_dirs=[os.path.join(SLEIGH_BUILD_DIR, "lib")],
+        library_dirs=[os.path.join(CMAKE_BUILD_DIR, "lib")],
     )
     return ffi
 
