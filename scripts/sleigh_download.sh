dca06c71080fb14fa07f3e6ff877f64c742e35e0
Matt Borgerson <contact@mborgerson.com>
2023-04-06 16:51:11 -0700

    sleigh_download.sh: Change output dir to sleigh

--- scripts/sleigh_download.sh
+++ scripts/sleigh_download.sh
@@ -6,7 +6,7 @@ git clone --depth=1 -b Ghidra_10.2.3_build https://github.com/NationalSecurityAg
 
 # We just need Makefile and $(LIBSLA_SOURCE) defined inside Makefile. Do it this
 # way to make sure we stay up to date with the list of required files.
-SLEIGH_SRC_DIR=sleigh_src
+SLEIGH_SRC_DIR=sleigh
 pushd ghidra_src/Ghidra/Features/Decompiler/src/decompile/cpp/
 echo -e "$SLEIGH_SRC_DIR:\n\tmkdir -p $SLEIGH_SRC_DIR\n\tcp \$(LIBSLA_SOURCE) Makefile $SLEIGH_SRC_DIR" >> Makefile
 make $SLEIGH_SRC_DIR
