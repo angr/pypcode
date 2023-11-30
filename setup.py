5683d574e42b2254ba6fb8dbbf55706b9b2ec747
Matt Borgerson <contact@mborgerson.com>
2023-11-30 14:24:43 -0700

    setup.py: Fix macOS arm64 wheel cross-compilation

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
+from glob import glob
 import os
 import platform
+import shutil
 import struct
 import subprocess
 import sys
@@ -9,7 +11,8 @@ from setuptools.command.build_ext import build_ext
 
 
 ROOT_DIR = os.path.abspath(os.path.dirname(__file__))
-BUILD_DIR = os.path.join(ROOT_DIR, "build", "native")
+TARGET_BUILD_DIR = os.path.join(ROOT_DIR, "build", "native")
+HOST_BUILD_DIR = os.path.join(ROOT_DIR, "build", "native", "host")
 
 
 class BuildExtension(build_ext):
@@ -23,12 +26,19 @@ class BuildExtension(build_ext):
         except OSError as exc:
             raise RuntimeError("Please install CMake to build") from exc
 
+        # When building pypcode wheels on GitHub Actions using cibuildwheel, ARCHFLAGS will be set
+        CROSS_COMPILING_FOR_MACOS_ARM64 = (
+            platform.system() == "Darwin" and platform.machine() == "x86_64" and "arm64" in os.getenv("ARCHFLAGS", "")
+        )
+        CROSS_COMPILING = CROSS_COMPILING_FOR_MACOS_ARM64
+
         install_pkg_root = os.path.abspath(os.path.join(ROOT_DIR if self.inplace else self.build_lib, "pypcode"))
         cmake_install_prefix = install_pkg_root
         cmake_config_args = [
             f"-DCMAKE_INSTALL_PREFIX={cmake_install_prefix}",
             f"-DPython_EXECUTABLE={sys.executable}",
         ]
+
         cmake_build_args = []
         if platform.system() == "Windows":
             is_64b = struct.calcsize("P") * 8 == 64
@@ -36,16 +46,46 @@ class BuildExtension(build_ext):
             cmake_build_args += ["--config", "Release"]
 
         # Build sleigh and pypcode_native extension
-        subprocess.check_call(["cmake", "-S", ".", "-B", BUILD_DIR] + cmake_config_args, cwd=ROOT_DIR)
+        target_cmake_config_args = cmake_config_args[::]
+        if CROSS_COMPILING_FOR_MACOS_ARM64:
+            target_cmake_config_args += [
+                "-DCMAKE_OSX_DEPLOYMENT_TARGET=10.14",
+                "-DCMAKE_OSX_ARCHITECTURES=arm64",
+            ]
+        subprocess.check_call(["cmake", "-S", ".", "-B", TARGET_BUILD_DIR] + target_cmake_config_args, cwd=ROOT_DIR)
         subprocess.check_call(
-            ["cmake", "--build", BUILD_DIR, "--parallel", "--verbose"] + cmake_build_args,
+            ["cmake", "--build", TARGET_BUILD_DIR, "--parallel", "--verbose"] + cmake_build_args,
             cwd=ROOT_DIR,
         )
-        subprocess.check_call(["cmake", "--install", BUILD_DIR], cwd=ROOT_DIR)
+
+        if CROSS_COMPILING:
+            # Also build a host version of sleigh to process .sla files
+            host_cmake_config_args = cmake_config_args[::]
+            subprocess.check_call(["cmake", "-S", ".", "-B", HOST_BUILD_DIR] + host_cmake_config_args, cwd=ROOT_DIR)
+            subprocess.check_call(
+                ["cmake", "--build", HOST_BUILD_DIR, "--parallel", "--verbose", "--target", "sleigh"]
+                + cmake_build_args,
+                cwd=ROOT_DIR,
+            )
+
+        bin_ext = ".exe" if platform.system() == "Windows" else ""
+        install_pkg_bin_dir = os.path.join(install_pkg_root, "bin")
+        sleigh_filename = "sleigh" + bin_ext
+
+        # Install extension and sleigh binary
+        if CROSS_COMPILING:
+            os.makedirs(install_pkg_bin_dir, exist_ok=True)
+            ext_path = glob(TARGET_BUILD_DIR + "/pypcode_native.*")[0]
+            shutil.copy(
+                os.path.join(TARGET_BUILD_DIR, sleigh_filename), os.path.join(install_pkg_bin_dir, sleigh_filename)
+            )
+            shutil.copy(ext_path, os.path.join(install_pkg_root, os.path.basename(ext_path)))
+        else:
+            subprocess.check_call(["cmake", "--install", TARGET_BUILD_DIR], cwd=ROOT_DIR)
 
         # Build sla files
-        bin_ext = ".exe" if platform.system() == "Windows" else ""
-        sleigh_bin = os.path.join(install_pkg_root, "bin", "sleigh" + bin_ext)
+        host_bin_root = HOST_BUILD_DIR if CROSS_COMPILING else install_pkg_bin_dir
+        sleigh_bin = os.path.join(host_bin_root, sleigh_filename)
         specfiles_dir = os.path.join(install_pkg_root, "processors")
         subprocess.check_call([sleigh_bin, "-a", specfiles_dir])
 
