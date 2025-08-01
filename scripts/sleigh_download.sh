93c090d0e82d6a4c8ef96ce18b490088c8fd308d
Matt Borgerson <contact@mborgerson.com>
2025-08-01 13:01:05 -0700

    Add updates from Ghidra 11.4.1

--- scripts/sleigh_download.sh
+++ scripts/sleigh_download.sh
@@ -2,12 +2,17 @@
 set -e
 set -x
 
-git clone --depth=1 -b Ghidra_11.3.2_build https://github.com/NationalSecurityAgency/ghidra.git ghidra_src
+git clone --depth=1 -b Ghidra_11.4.1_build https://github.com/NationalSecurityAgency/ghidra.git ghidra_src
 
 # We just need Makefile and $(LIBSLA_SOURCE) defined inside Makefile. Do it this
 # way to make sure we stay up to date with the list of required files.
 SLEIGH_SRC_DIR=sleigh
 pushd ghidra_src/Ghidra/Features/Decompiler/src/decompile/cpp/
+
+# Touch fake dependency files recently removed upstream. Not having these triggers build steps.
+mkdir -p com_opt com_dbg
+touch com_opt/depend com_dbg/depend
+
 echo -e "$SLEIGH_SRC_DIR:\n\tmkdir -p $SLEIGH_SRC_DIR\n\tcp \$(LIBSLA_SOURCE) Makefile $SLEIGH_SRC_DIR" >> Makefile
 make $SLEIGH_SRC_DIR
 SLEIGH_SRC_DIR=${PWD}/${SLEIGH_SRC_DIR}
