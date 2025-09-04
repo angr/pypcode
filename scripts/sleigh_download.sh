a95983e80313604e1cd5e3115aeea77d94b7be55
Matt Borgerson <contact@mborgerson.com>
2025-09-04 12:22:02 -0700

    sleigh_download.sh: Download to tag dir

--- scripts/sleigh_download.sh
+++ scripts/sleigh_download.sh
@@ -2,12 +2,14 @@
 set -e
 set -x
 
-git clone --depth=1 -b Ghidra_11.4.1_build https://github.com/NationalSecurityAgency/ghidra.git ghidra_src
+TAG=11.4.2
+GHIDRA_SRC_DIR=ghidra_src_${TAG}
+git clone --depth=1 -b Ghidra_${TAG}_build https://github.com/NationalSecurityAgency/ghidra.git ${GHIDRA_SRC_DIR}
 
 # We just need Makefile and $(LIBSLA_SOURCE) defined inside Makefile. Do it this
 # way to make sure we stay up to date with the list of required files.
 SLEIGH_SRC_DIR=sleigh
-pushd ghidra_src/Ghidra/Features/Decompiler/src/decompile/cpp/
+pushd ${GHIDRA_SRC_DIR}/Ghidra/Features/Decompiler/src/decompile/cpp/
 
 # Touch fake dependency files recently removed upstream. Not having these triggers build steps.
 mkdir -p com_opt com_dbg
@@ -18,5 +20,6 @@ make $SLEIGH_SRC_DIR
 SLEIGH_SRC_DIR=${PWD}/${SLEIGH_SRC_DIR}
 popd
 
-mv $SLEIGH_SRC_DIR .
-mv ghidra_src/Ghidra/Processors processors
+mkdir ${TAG}
+mv $SLEIGH_SRC_DIR ${TAG}
+mv ${GHIDRA_SRC_DIR}/Ghidra/Processors ${TAG}/processors
