74610c32f27ea6ce92441c9cef119b87acb7f960
Matt Borgerson <contact@mborgerson.com>
2025-10-09 12:17:36 -0700

    tests: Improve __main__.py testing

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -14,7 +14,10 @@ from pypcode.__main__ import main
 
 def run_cli(*args):
     with mock.patch("sys.argv", [sys.executable, *args]), mock.patch("sys.stdout", new=io.StringIO()) as fake_out:
-        main()
+        try:
+            main()
+        except SystemExit:
+            pass
         return fake_out.getvalue()
 
 
@@ -23,11 +26,22 @@ class TestCli(unittest.TestCase):
     Test the pypcode module command line interface
     """
 
-    def test_cli_list(self):
+    def test_language_list(self):
         output = run_cli("-l")
         assert "x86:LE:64:default" in output
 
-    def test_cli_trans(self):
+    def test_language_suggestions(self):
+        output = run_cli("x86", "_")
+        assert 'Language "x86" not found.' in output
+        assert "Suggestions:" in output
+        assert "x86:LE:32:default" in output
+        assert "x86:LE:64:default" in output
+
+    def test_language_no_suggestions(self):
+        output = run_cli("xyz", "_")
+        assert "Suggestions:" not in output
+
+    def test_translate(self):
         with tempfile.NamedTemporaryFile(delete=False) as tf:
             tf.write(base64.b64decode("McA5xnYRSInBg+EfigwKMAwHSP/A6+vD"))
             tf.close()
@@ -36,6 +50,18 @@ class TestCli(unittest.TestCase):
         try:
             output = run_cli("x86:LE:64:default", path)
             assert "0x17/1: RET" in output
+        finally:
+            os.unlink(path)
+
+    def test_failed_translation(self):
+        with tempfile.NamedTemporaryFile(delete=False) as tf:
+            tf.write(b"\x40\x40")
+            tf.close()
+            path = tf.name
+
+        try:
+            output = run_cli("x86:LE:64:default", path)
+            assert "An error occurred" in output
         finally:
             os.unlink(path)
 
